8de41a91e335d5aac621d35cd0a3f314f2cdcdd9
Kacper <kacpermazur93@gmail.com>
2018-03-17 16:23:53 +0100

    status3

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 'use strict';
 
-//Zadanie1
+
 document.addEventListener('DOMContentLoaded', function () {
     var showMenuDOM = document.querySelector('.for-dropdown');
     var menuDOM = document.querySelector('.dropdown');
     
     
+    //Zadanie1
     showMenuDOM.addEventListener('mouseover', function () {
         menuDOM.style.display = 'block';
         //console.log(showMenuDOM, menuDOM);
@@ -16,7 +17,24 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     
+    //Zadanie 2
+    var buttons = document.querySelectorAll('.read-more');
+   
+    function showHide() {
+        var textArea = this.previousElementSibling;
+
+        if (textArea.style.display === 'none' || textArea.style.display === '') {
+            textArea.style.display = 'block';
+            this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+        } else {
+            textArea.style.display = 'none';
+            this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+        }
+    }   
     
+    for (var i = 0; i <= buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide);
+    }
     
 });
 
